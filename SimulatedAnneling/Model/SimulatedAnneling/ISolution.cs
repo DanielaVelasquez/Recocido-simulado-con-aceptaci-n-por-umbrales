bbc8d059d2a68bbceff0c079b9514a5d4b639310
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-30 09:16:28 -0500

    Creación interface ISolution

---
+++ SimulatedAnneling/Model/SimulatedAnneling/ISolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedAnneling.Model.SimulatedAnneling
+{
+    public interface ISolution
+    {
+        ///<summary>
+        ///Calcula la función de costo de una posible solución
+        /// @return valor del costo de función
+        /// </summary>
+        ///<returns>
+        ///Función de costo asociada a la solución
+        ///</returns>
+        double calculateCostFunction();
+        ///<summary>
+        ///Obtiene un vecino a la solución actual a partir de un elemento random
+        ///</summary>
+        ///<param name="random">objeto permite aletoriedad iniciado con una semilla</param>
+        ///<returns>Solución vecina a la solución actual</returns>
+        ISolution getNeighbour(Random random);
+    }
+}
