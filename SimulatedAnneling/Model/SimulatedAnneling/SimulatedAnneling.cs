057f8ef804290e83d23bb090a217ac0a24087bca
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-30 10:19:19 -0500

    Creación inicial de atributos de SimulatedAnneling

--- SimulatedAnneling/Model/SimulatedAnneling/SimulatedAnneling.cs
+++ SimulatedAnneling/Model/SimulatedAnneling/SimulatedAnneling.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace SimulatedAnneling.Model.SimulatedAnneling
 {
-    public class SimulatedAnneling
+    public class SimulatedAnneling<T> where T: ISolution
     {
+        /**-------------------------------------------------------------------------------------------
+         * Atributos
+         *--------------------------------------------------------------------------------------------
+         **/
         /// <summary>
-        /// Temperatura indica 
+        /// Temperatura permite explorar soluciones fuera de un mínimo local
         /// </summary>
         private int temperature;
+        /// <summary>
+        /// Semilla utilizada para aletoriedad de las soluciones
+        /// </summary>
+        private int seed;
+        /// <summary>
+        /// Objeto genera números aletorios con base en la semilla
+        /// </summary>
+        private Random random;
+        /// <summary>
+        /// Factor de enfriamiento del sistema, determina que tan rápido o lento
+        /// la temperatura disminuyendo
+        /// </summary>
+        private float coolingFactor;
+
+        
+
     }
 }
