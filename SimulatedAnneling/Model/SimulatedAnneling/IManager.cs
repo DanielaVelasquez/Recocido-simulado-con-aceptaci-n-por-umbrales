711d9e3baf94eb16ad0e81b37fd577890bacbc33
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-30 09:37:16 -0500

    Creacion del IManager con comentarios

---
+++ SimulatedAnneling/Model/SimulatedAnneling/IManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedAnneling.Model.SimulatedAnneling
+{
+    /// <summary>
+    /// Modela el administrador del problema, encargado de proveer
+    /// información general asociada al problema a resolver por ejemplo
+    /// generar una solución aletoria inicial
+    /// </summary>
+    public interface IManager
+    {
+        /// <summary>
+        /// Crea una solución aletoria del problema
+        /// </summary>
+        /// <param name="random">Objeto crea de forma aletoria una solución, iniciado con una semilla</param>
+        /// <returns>Una solución aletoria asociada al problema</returns>
+        ISolution getRamdomSolution(Random random);
+    }
+}
