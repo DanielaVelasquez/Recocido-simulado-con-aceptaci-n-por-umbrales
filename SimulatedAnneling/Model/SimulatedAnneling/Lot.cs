f3920d341b8bdf5e8902b0aa16f37ea1d1ecdb22
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-30 11:36:01 -0500

    Configuración del lote

---
+++ SimulatedAnneling/Model/SimulatedAnneling/Lot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SimulatedAnneling.Model.SimulatedAnneling
+{
+    /// <summary>
+    /// Representa un lote producido al generar el recocido simulado
+    /// </summary>
+    public class Lot<T> where T : ISolution
+    {
+        /**-------------------------------------------------------------------------------------------
+         * Atributos
+         *--------------------------------------------------------------------------------------------
+         **/
+        ///<summary>
+        ///Soluciones aceptadas por un lote
+        ///</summary>
+        private ArrayList solutions;
+        /// <summary>
+        /// Temperatura en la cual fue generado el lote
+        /// </summary>
+        private int temperature;
+        /// <summary>
+        /// Determina si el lote se generó completo o se detuvo al alcanzar
+        /// el máximo número de iteraciones
+        /// </summary>
+        private Boolean finished;
+
+        /**-------------------------------------------------------------------------------------------
+         * Métodos
+         *--------------------------------------------------------------------------------------------
+         **/
+        /// <summary>
+        /// Constructo de un lote
+        /// </summary>
+        /// <param name="nTemperature">Temperatura a la cual se está generando el lote</param>
+        public Lot(int nTemperature)
+        {
+            temperature = nTemperature;
+            finished = false;
+            solutions = new ArrayList();
+        }
+        /// <summary>
+        /// Adiciona una nueva solución al lote
+        /// </summary>
+        /// <param name="solution">Nueva solución del lote</param>
+        public void addSolution(T solution)
+        {
+            solutions.Add(solution);
+        }
+    }
+}
