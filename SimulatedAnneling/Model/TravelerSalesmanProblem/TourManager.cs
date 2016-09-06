7c216c7409470ee98c5ecbf02e2d4a8417e404ec
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-09-06 18:14:35 -0500

    agregó modelo de clases en uml

---
+++ SimulatedAnneling/Model/TravelerSalesmanProblem/TourManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimulatedAnneling.Model.SimulatedAnneling;
+
+namespace SimulatedAnneling.Model.TravelerSalesmanProblem
+{
+    /// <summary>
+    /// Objeto dirige el problema, cononoce todas las ciudades y es capaz de de dar un información sobre todas
+    /// ellas, es capaz de generar soluciones aletorias a partir de ellas.
+    /// </summary>
+    public class TourManager:IManager
+    {
+
+    }
+}
